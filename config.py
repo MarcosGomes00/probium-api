b00fb5ec42b34995f36c5c1e2ce132c2639378cd
Marcos Gomes <marcostradding@gmail.com>
2026-03-06 17:26:04 -0300

    update probium automation

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
+import os
+
+
 class Config:
+
+    # DATABASE (Railway)
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
     # APIs
     ODDS_API_KEY = "6a1c0078b3ed09b42fbacee8f07e7cc3"
     API_FOOTBALL_KEY = "a1b4dc55ed3248a09e8b8582e4dbc0c9"
+
 
     # TELEGRAM
     TELEGRAM_BOT_1 = "8725909088:AAGQMNr-9RVQB7hWmePCLmm0GwaGuzOVy-A"
